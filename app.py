ea39c06e2f9187699b57ec703ee88e08c2bd19de
Samuel Knobel <samuel.knobel@sonova.com>
2024-10-09 21:56:26 +0200

    remove unused import statements

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import altair as alt
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 
 st.set_page_config(layout="wide")
 regions = ["LATAM", "EMEA", "NA", "APAC"]
